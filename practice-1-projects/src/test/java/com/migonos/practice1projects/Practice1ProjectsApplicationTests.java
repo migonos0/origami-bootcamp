b1510316dc652c37fb836947319d1f284ad7fbaa
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-12-12 15:52:28 -0500

    Created base project of the first pracitce.

---
+++ practice-1-projects/src/test/java/com/migonos/practice1projects/Practice1ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migonos.practice1projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice1ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
